9f51680fbd580635d06cad62feba3beaf88bfe71
MacuMacu12 <124742762+MacuMacu12@users.noreply.github.com>
2025-09-02 19:56:52 -0400

    Adding User Input
    adding user ability to input side lengths

--- Figures/src/shapes/Main.java
+++ Figures/src/shapes/Main.java
@@ -1,10 +1,25 @@
 package shapes;
 
+import java.util.Scanner;
+
 public class Main {
 
+static Scanner input = new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		float sideLength = 0;
+		
+		System.out.println("Please enter a sidelength!");
+		sideLength = input.nextFloat();
+		
+		Square square1 = new Square();
+		Square square2 = new Square(sideLength);
+		
+		
+		System.out.println(square2.getArea());
+		
+		
 	}
 
 }
--- Figures/src/shapes/Square.java
+++ Figures/src/shapes/Square.java
@@ -2,9 +2,25 @@ package shapes;
 
 public class Square {
 
-	public static void main(String[] args) {
-		System.out.println("hello");
-
+	private float sidelength;
+	private float area;
+	
+	public Square() {
+		
+		sidelength = 1.0f;
+		area = 1.0f;
+		
+	}
+	
+	public Square(float sideLength) {
+		sideLength = sideLength;
+		area = sideLength * sideLength;
+		
+	}
+	
+	public float getArea() {
+		
+		return area;
 	}
 
 }
